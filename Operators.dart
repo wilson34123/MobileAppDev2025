622aa2857e3e1929d126aaf7f9610166926fd39f
wilson34123 <wilsonhallson@gmail.com>
2025-07-06 12:51:15 +0200

    Initial commit

---
+++ Operators.dart
@@ -0,0 +1,36 @@
+void main() {
+
+
+  // Arithmetic Operators
+print("Arithmetic Operators");
+  int a = 10;
+  int b = 3;
+
+  print(a + b);    // 13
+  print(a - b);    // 7
+  print(a * b);    // 30
+  print(a / b);    // 3.333...
+  print(a % b);    // 1
+  print(a ~/ b);   // 3 (integer division)
+
+
+  // Comparison Operators
+print("Comparison Operator");
+  int x = 5;
+  int y = 10;
+
+  print(x == y);  // false
+  print(x != y);  // true
+  print(x < y);   // true
+
+  // Logical Operators
+
+  print("Logical Operators");
+
+  bool M = true;
+  bool N = false;
+
+  print(M && N);  // false
+  print(M || N);  // true
+  print(!M);      // false
+}
